ece5a11ae0e593ff63ba903ea70ffb57476298ae
kRxZykRxZy <192328467+kRxZykRxZy@users.noreply.github.com>
2025-06-08 11:40:09 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,77 @@
+import scratchattach as sa
+import requests
+import urllib.request
+from PIL import Image
+import json, os, random, math, time
+from keep_alive import keep_alive
+
+def log(*args):
+    print(f"[{round(time.time())}]", *args)
+
+def convertToNumber(s):
+    return int.from_bytes(s.encode(), 'little')
+
+def convertFromNumber(n):
+    return n.to_bytes(math.ceil(n.bit_length() / 8), 'little').decode()
+
+session_ps = os.environ.get("SCRATCH_PS")
+session = sa.login('Dev-Server', session_ps)  # replace with your session_id and username
+cloud1 = session.connect_cloud(992640266)  # replace with your project id
+client1 = cloud1.requests()
+
+@client1.request
+def ping():  # called when client receives request
+    log("Ping request received")
+    return "pong"
+
+@client1.request
+def get_pfp(username):
+    try:
+        user_id = sa.get_user(username).id()
+    except:
+        return "User Not Found"
+    img_url = f"https://uploads.scratch.mit.edu/get_image/user/{user_id}_200x200.png"
+    log(f"Image url: {img_url}")
+    image_name = f"pfp{convertToNumber(username)}.png"
+    urllib.request.urlretrieve(img_url, f"/tmp/{image_name}")
+    log(f"Image stored in /tmp/{image_name}")
+    return image_name
+
+@client1.request
+def get_image_piece(img_id, y_offset, img_size, username):
+    img_id = img_id.replace("/", "").replace("\\", "")
+    try:
+        img = Image.open(f"/tmp/{img_id}").convert("RGBA")
+    except:
+        log("Failed to get image data from", img_id, "by", username)
+        return "Error getting image data"
+    img = img.resize((int(img_size), int(img_size)))
+    width, height = img.size
+    pixels = img.load()
+
+    amount = 10
+    colors = []
+    for y in range(int(y_offset), int(y_offset) + int(amount)):
+        for x in range(width):
+            r, g, b, a = pixels[x, y]
+            color = a * 16777216 + r * 65536 + g * 256 + b
+            colors.append(color)
+    log(username, 'requested image piece for image "' + img_id + '" with y offset', y_offset)
+    return colors
+
+@client1.request
+def done(img_id):
+    try:
+        os.remove(f'/tmp/{str(img_id)}')
+        log("Removing file", img_id)
+        return "Done"
+    except:
+        return "Error deleting file"
+
+@client1.event
+def on_ready():
+    log("Request handler is running")
+
+keep_alive()
+client1.start()
+log("Started stuff")
